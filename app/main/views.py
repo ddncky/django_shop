f934c6859df3448bfea4c0a08e246c430690eb09
Dmitriy Dudnytskyi <montekristo705@gmail.com>
2024-09-22 22:08:09 +0300

    Create index and about views (to be changed)

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "title": "Home", "content": "Главная страница магазина - HOME"
+        }
+    return render(request=request, template_name="main_templates/index.html", context=context)
+
+
+def about(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("About page")
