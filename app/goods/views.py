594182df71860da56bd4a02484bbe494094a8eae
Dmitriy Dudnytskyi <montekristo705@gmail.com>
2024-09-23 16:45:43 +0300

    Создали вью, подключили локально, подключили в главном приложении;

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def catalog(request: HttpRequest):
+    return render(request=request, template_name="goods_templates/catalog.html")
+
+
+def product(request: HttpRequest) -> HttpResponse:
+    return render(request=request, template_name="goods_templates/product.html")
