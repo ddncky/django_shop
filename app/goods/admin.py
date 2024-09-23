f873aa4c0247c877170b1cfe9a48ce4140cb750c
Dmitriy Dudnytskyi <montekristo705@gmail.com>
2024-09-24 00:16:34 +0300

    Создал модели категорий и товаров. Подключил к админке;

--- app/goods/admin.py
+++ app/goods/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+# admin.site.register(Category)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("name",)
+        }
+    
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("name",)
+        }
