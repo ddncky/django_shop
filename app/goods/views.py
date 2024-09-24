51a0aba9ed7f70e9e14c3c9221212108a2ae5f45
Dmitriy Dudnytskyi <montekristo705@gmail.com>
2024-09-24 20:05:00 +0300

    Paginations setting;

--- app/goods/views.py
+++ app/goods/views.py
@@ -5,14 +5,16 @@ from django.core.paginator import Paginator
 from . models import Product
 
 
-def catalog(request: HttpRequest, category_slug, page=1):
+def catalog(request: HttpRequest, category_slug):
+    page = request.GET.get("page", 1)
+
     if category_slug == "all":
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home - Каталог",
