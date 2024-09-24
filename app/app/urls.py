8c88e99501ebaa83a1ca9268544a026fd4a934a2
Dmitriy Dudnytskyi <montekristo705@gmail.com>
2024-09-24 13:19:34 +0300

    Оптимизировали подлючение изображений;

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from .settings import DEBUG
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
+
+from . import settings
 
 
 urlpatterns = [
@@ -27,7 +29,8 @@ urlpatterns = [
     path("catalog/", include("goods.urls", namespace="catalog")),
 ] 
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
