85c17e8a5ce5bcd838a6ed935f7ec028c8c74951
Dmitriy Dudnytskyi <montekristo705@gmail.com>
2024-09-29 21:03:21 +0300

    Make login form and login view;

--- app/users/views.py
+++ app/users/views.py
@@ -1,11 +1,29 @@
-from django.http import HttpResponse, HttpRequest
+from django.contrib import auth
+from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 
 def login(request: HttpRequest):
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+
     context = {
-        "title": "Home - Авторизация"
+        "title": "Home - Авторизация",
+        "form": form,
     }
+
     return render(request=request, template_name="users_templates/login.html", context=context)
 
 
